20f58c7e621900cd9b38c8031e290e7a3eec0af0
basarat <basaratali@gmail.com>
2018-08-08 12:20:38 +1000

    ready

---
+++ 01/src/checkboxWithLabel.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+export class CheckboxWithLabel extends React.Component<{
+  id: string,
+  labelOn: string,
+  labelOff: string
+}, {
+    isChecked: boolean
+  }> {
+  constructor(props) {
+    super(props);
+    this.state = { isChecked: false };
+  }
+
+  onChange = () => {
+    this.setState({ isChecked: !this.state.isChecked });
+  }
+
+  render() {
+    return (
+      <label id={this.props.id}>
+        <input
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.onChange}
+        />
+        {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+      </label>
+    );
+  }
+}
