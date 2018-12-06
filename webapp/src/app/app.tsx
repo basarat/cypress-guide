62388147151520386166bf2f7db7a72c4a358225
basarat <basaratali@gmail.com>
2018-12-07 10:37:31 +1100

    better

--- webapp/src/app/app.tsx
+++ webapp/src/app/app.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { normalize, setupPage } from "csstips";
+
+normalize();
+setupPage('#root');
+
 const Hello: React.SFC<{ compiler: string, framework: string }> = (props) => {
   return (
     <div>
