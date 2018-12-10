226e3735cc66b3b3bf0ba8b7e09b5b19ad3597fc
basarat <basaratali@gmail.com>
2018-12-10 13:55:37 +1100

    server

---
+++ webapp/src/main.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import low from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+
+/** 
+ * Setup server 
+ */
+const app = express();
+
+/** 
+ * Setup db 
+ */
+const adapter = new FileSync('db.json')
+const db = low(adapter);
+
+db.defaults({ todos: [] })
+  .write();
+
+/** 
+ * API server 
+ */
+app.use(express.json());
