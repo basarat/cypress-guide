97cdcfd073f9a25265144ae1d0277e7b39b3ca4c
basarat <basaratali@gmail.com>
2019-01-07 15:15:10 +1100

    added add api endpoint

--- webapp/src/server/server.ts
+++ webapp/src/server/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import cors from 'cors';
 import low from 'lowdb';
+import uuid from 'uuid/v4';
 import FileSync from 'lowdb/adapters/FileSync';
 import { TodoItem } from '../common/types';
 
@@ -8,7 +10,7 @@ import { TodoItem } from '../common/types';
  */
 type DBSchema = {
   items: TodoItem[]
-} 
+}
 const adapter = new FileSync<DBSchema>('db.json', {
   defaultValue: {
     items: []
@@ -22,8 +24,20 @@ const db = low(adapter);
 const app = express();
 const api = express.Router();
 
+api.use(cors(), express.json());
 api.get('/all', (_, res) => {
   res.send({ todos: db.get('items') });
+});
+api.post('/add', (req, res) => {
+  const id = uuid();
+  db.get('items')
+    .push({
+      id: id,
+      completed: false,
+      message: req.body.message
+    })
+    .write();
+  res.send({ id });
 });
 
 app.use('/api', api);
